752c6fab5a0db05eab2d377578d367fc1474e0ad
Julian V <julivehs@gmail.com>
2023-11-16 09:55:09 +0100

    Fix Main Class

--- src/main/java/h07/Main.java
+++ src/main/java/h07/Main.java
@@ -39,28 +39,20 @@ public class Main {
     }
 
 
-    /**
-     * Main entry point in executing the program.
-     *
-     * @param args program arguments, currently ignored
-     */
-    public static void main(String[] args) throws InterruptedException {
-
+    private static void test_h22(){
         System.out.println("H2.2: ");
         System.out.println(testNormal().map(TEST_STRING));
         System.out.println(testLambda().map(TEST_STRING));
         System.out.println(testMethodReference().map(TEST_STRING));
         System.out.println();
+    }
 
-
-        // Log
-        System.out.println("H4.X:");
-        Log log = new MaintenanceLog();
+    private static void test_h4() throws InterruptedException {
+        System.out.println("H4:");
+        Log log = new NormalLog(); //new MaintenanceLog();
         log.log(1, "Hallo FoPler!");
         log.log(6, "Error: Code 6 received!");
         log.log(3, "Warnung: Diese Uebung ist hiermit beendet\nOver and out!");
-
-
 
 
         PowerPlant powerPlant = new PowerPlant(log, new Random(42), 10);
@@ -69,7 +61,29 @@ public class Main {
             powerPlant.check(i / 100.0);
             Thread.sleep(1500);
         }
+    }
 
 
+    private static void test_h5() throws InterruptedException {
+        System.out.println("H5:");
+        Log log = new NormalLog(); //new MaintenanceLog();
+        PowerPlant powerPlant = new PowerPlant(log, new Random(42), 10);
+
+        for(int i = 0; i < 10; i++){
+            powerPlant.check(i / 100.0);
+            Thread.sleep(1500);
+        }
+    }
+
+
+    /**
+     * Entry Point
+     * @param args Parameter
+     * @throws InterruptedException Exception for sleeping Thread
+     */
+    public static void main(String[] args) throws InterruptedException {
+        test_h22();
+        test_h4();
+        test_h5();
     }
 }
